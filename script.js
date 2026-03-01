7ddc6dd47957726a66924cb483ffaab6e7f47af2
Maria Eduarda Gouvea Alves <gouveamaria2005@gmail.com>
2026-03-01 15:46:19 -0300

    site de fofoca

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Inicializando Script 01")
+
+function ler() {
+    console.log("Executando função ler...")
+
+}
+
+console.log("Fechando Execução do Script 02")
+
+function salvar() {
+    alert("Gravando Dados.")
+ }
+
+function cancelar() {
+    alert("Dados apagados.")
+ }
